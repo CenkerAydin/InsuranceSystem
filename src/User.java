74e1fadecd32460c48dfb7db039694908a075e28
Cenker Aydın <cenkeraydincse@gmail.com>
2023-07-03 18:20:15 +0300

    Added

---
+++ src/User.java
@@ -0,0 +1,78 @@
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class User {
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+    private String profession;
+    private int age;
+    private ArrayList<Address> address;
+    private Date lastLogin;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public ArrayList<Address> getAddress() {
+        return address;
+    }
+
+    public void setAddress(ArrayList<Address> address) {
+        this.address = address;
+    }
+
+    public Date getDate() {
+        return lastLogin;
+    }
+
+    public void setDate(Date date) {
+        this.lastLogin = date;
+    }
+}
